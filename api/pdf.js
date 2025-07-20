ca40e692511d7d1407d9b7cc8cd45460debc3219
root <952739568@qq.com>
2025-07-21 00:34:23 +0800

    test

--- api/pdf.js
+++ api/pdf.js
@@ -13,7 +13,7 @@ export default async function handler(req, res) {
 		return res.status(400).send("Missing URL");
 	}
 
-	
+	fileUrl = decodeURIComponent(fileUrl);
 
 	try {
 		const response = await fetch(fileUrl, {
