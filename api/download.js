8db2183e53b2e8a1cbb66af3d55ac4c95e96a359
root <952739568@qq.com>
2025-07-21 10:24:48 +0800

    test

---
+++ api/download.js
@@ -0,0 +1,57 @@
+// /api/proxy.js
+export default async function handler(req, res) {
+	
+	const arr = req.url.split("/api/download/");
+	
+	if(arr.length < 1) {
+		return res.status(400).send("Missing URL");
+	}
+	
+	let fileUrl = arr[1];
+
+	if (!fileUrl) {
+		return res.status(400).send("Missing URL");
+	}
+
+	fileUrl = decodeURIComponent(fileUrl);
+
+	try {
+		const response = await fetch(fileUrl, {
+		  method: 'GET',
+		  headers: {
+			'User-Agent': 'Mozilla/5.0',
+			// 可以加入身份验证信息等头部
+		  },
+		});
+
+		if (!response.ok) {
+		  return res.status(400).send("获取文件错误: " + response.status + " url: " + fileUrl);
+		}
+		
+		if (response.status === 302) {
+		  const newUrl = response.headers.get('location'); // 获取重定向的 URL
+
+		  if (newUrl) {
+			// 使用 302 跳转到新 URL
+			return res.redirect(302, '/api/download?url=' + newUrl);
+		  } else {
+			res.status(400).send('Redirect URL not found');
+		  }
+		}
+
+		// 使用 arrayBuffer() 获取文件的二进制数据
+		const arrayBuffer = await response.arrayBuffer();
+
+		// 将 ArrayBuffer 转为 Buffer，并返回文件
+		const buffer = Buffer.from(arrayBuffer);
+
+		// 设置响应头，返回 PDF 文件
+		res.setHeader('Content-Type', response.headers.get('content-type') || 'application/octet-stream');
+		res.setHeader('Content-Disposition', 'attachment; filename="downloaded-file"');
+		res.status(200).send(buffer);  // 返回 PDF 文件内容
+
+	} catch (error) {
+		console.error('Error fetching file:', error);
+		res.status(400).json({ error: 'Failed to proxy file', details: error.message });
+	}
+}
